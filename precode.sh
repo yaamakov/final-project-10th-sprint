29169189fc57bf5bd0450db824ccb0c56ed376d5
Makov Andrew <machok06@yandex.ru>
2025-06-13 14:13:41 +0300

    check linux command

--- precode.sh
+++ precode.sh
@@ -40,7 +40,7 @@ find . -name "*.txt" -type f > dir1/summary.txt
 cat dir2/list.txt >> dir1/summary.txt
 
 # определяем переменную окружения NAME со значением "Всем студентам"
-export NAME="Всем студентам"
+NAME="Всем студентам"
 
 # запускаем task/dir2/hello.sh с переменной окружения NAME в качестве аргумента
 # вывод скрипта должен дописаться в файл task/dir1/summary.txt
